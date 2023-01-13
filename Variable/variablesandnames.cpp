b5bb2cfb927c4e0f51d4c36f3732958fd7081c9c
vbear1223 <118244619+vbear1223@users.noreply.github.com>
2023-01-13 15:00:38 +0800

    Update variablesandnames.cpp

--- Variable/variablesandnames.cpp
+++ Variable/variablesandnames.cpp
@@ -1,1 +1,26 @@
 
+//Basic and Printing Variables and Names Valerie (7 points)
+#include <iostream>
+using namespace std;
+
+int main() {
+        int cars, drivers, passengers, cars_not_driven, cars_driven;
+        double space_in_a_car, carpool_capacity, average_passengers_per_car;
+
+        cars = 100;
+        space_in_a_car = 4.0;
+        drivers = 30;
+        passengers = 90;
+        cars_not_driven = cars - drivers;
+        cars_driven = drivers;
+        carpool_capacity = cars_driven * space_in_a_car;
+        average_passengers_per_car = passengers / cars_driven;
+
+
+        cout << "There are " << cars << " cars available."<<"\n";
+        cout << "There are only " << drivers << " drivers available."<<"\n";
+        cout <<  "There will be " << cars_not_driven << " empty cars today\n";
+        cout <<  "We can transport " << carpool_capacity << " people today\n";
+        cout <<  "We have " << passengers << " to carpool today.\n";
+        cout <<  "We need to put about " << average_passengers_per_car << " in each car.\n";
+}
